ef79149e8ca2958da30d78bfbb89793f5a26511e
Sriman Kundavaram <srimank@berkeley.edu>
2024-09-24 04:23:52 +0000

    Progress on WordGuesser App

--- lib/wordguesser_game.rb
+++ lib/wordguesser_game.rb
@@ -1,25 +1,61 @@
 class WordGuesserGame
 
-  # add the necessary class methods, attributes, etc. here
-  # to make the tests in spec/wordguesser_game_spec.rb pass.
+  attr_accessor :word, :guesses, :wrong_guesses
 
-  # Get a word from remote "random word" service
 
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
   end
 
-  # You can test it by installing irb via $ gem install irb
-  # and then running $ irb -I. -r app.rb
-  # And then in the irb: irb(main):001:0> WordGuesserGame.get_random_word
-  #  => "cooking"   <-- some random word
+
+  def guess(letter)
+
+    raise ArgumentError if letter.nil? || letter.empty? || !letter.match?(/[a-zA-Z]/)
+
+    letter = letter.downcase
+
+
+    return false if @guesses.include?(letter) || @wrong_guesses.include?(letter)
+
+
+    if @word.include?(letter)
+      @guesses += letter
+    else
+      @wrong_guesses += letter
+    end
+
+    true
+  end
+
+
+  def word_with_guesses
+    if @guesses.empty?
+
+      return '-' * @word.length
+    else
+
+      @word.gsub(/[^#{@guesses}]/, '-')
+    end
+  end
+
+
+  def check_win_or_lose
+    if @wrong_guesses.length >= 7
+      :lose
+    elsif @word.chars.all? { |char| @guesses.include?(char) }
+      :win
+    else
+      :play
+    end
+  end
+
+
   def self.get_random_word
-    require 'uri'
     require 'net/http'
+    require 'uri'
     uri = URI('http://randomword.saasbook.info/RandomWord')
-    Net::HTTP.new('randomword.saasbook.info').start { |http|
-      return http.post(uri, "").body
-    }
+    Net::HTTP.post(uri, "").body
   end
-
 end
